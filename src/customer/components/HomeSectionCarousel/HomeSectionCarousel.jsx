fb5379518cedc92737565151cc4f18de49a53df4
ginuraB <uniginura@gmail.com>
2024-12-14 22:11:28 +0530

    second day of work nothing much home section buttons added

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,29 +1,69 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import { Button } from '@mui/material';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const HomeSectionCarousel = () => {
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 4 },
+        1024: { items: 5.5 },
     };
-    const items = [1, 1, 1,1 , 1].map(() => <HomeSectionCard />);
+    const items = [1, 1, 1, 1, 1].map(() => <HomeSectionCard />);
 
-  return (
-    <div>
-        <AliceCarousel
-              
-              items={items}
-              disableButtonsControls
-              autoPlay
-              autoPlayInterval={1000}
-              infinite
-              responsive={responsive}
-                
-            />
-    </div>
-  )
+    return (
+        <div className='relative px-4 lg:px-8'>
+
+            <div className='relative p-5'>
+                <AliceCarousel
+
+                    items={items}
+                    disableButtonsControls
+                    infinite
+                    responsive={responsive}
+
+                />
+
+
+<button 
+    variant='contained' 
+    className='z-50' 
+    style={{ 
+        position: 'absolute', 
+        top: '8rem', 
+        left: '0rem', 
+        backgroundColor: 'white', 
+        color: 'black', 
+        transition: 'background-color 0.3s ease' 
+    }} 
+    onMouseEnter={(e) => e.currentTarget.style.backgroundColor = 'lightblue'} 
+    onMouseLeave={(e) => e.currentTarget.style.backgroundColor = 'white'}
+>
+    <ArrowLeftIcon />
+</button>
+
+<button 
+    variant='contained' 
+    className='z-50' 
+    style={{ 
+        position: 'absolute', 
+        top: '8rem', 
+        right: '0rem', 
+        backgroundColor: 'white', 
+        color: 'black', 
+        transition: 'background-color 0.3s ease' 
+    }} 
+    onMouseEnter={(e) => e.currentTarget.style.backgroundColor = 'lightblue'} 
+    onMouseLeave={(e) => e.currentTarget.style.backgroundColor = 'white'}
+>
+    <ArrowRightIcon />
+</button>
+            </div>
+
+        </div>
+    )
 }
 
 export default HomeSectionCarousel
