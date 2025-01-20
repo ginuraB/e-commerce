7dd0e4faab1ceb45f95dcc053ba310a302dc9ca1
ginuraB <uniginura@gmail.com>
2025-01-21 00:33:27 +0530

    20/01/2025

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
 import HomePage from './customer/Pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
+import Product from './customer/components/Product/Product';
+
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       
       <Navigation/>
       <div>
-        <HomePage/>
+        <Product/>
+        {/**<HomePage/> */}
       </div>
       <Footer/>
     </div>
