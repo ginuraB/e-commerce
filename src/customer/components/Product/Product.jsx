286b9135c52f9527ae5730f38abc013f2be36849
ginuraB <uniginura@gmail.com>
2025-02-01 13:12:38 +0530

    01/02/2025

--- src/customer/components/Product/Product.jsx
+++ src/customer/components/Product/Product.jsx
@@ -18,9 +18,10 @@ import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from
 import { mens_kurta } from '../../../Data/mens_kurta'
 import ProductCard from './ProductCard'
 import { filters } from './FilterData'
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 
 const sortOptions = [
-  
+
   { name: 'Price: Low to High', href: '#', current: false },
   { name: 'Price: High to Low', href: '#', current: false },
 ]
@@ -119,7 +120,7 @@ export default function Product() {
                     </li>
                   ))}
                 </ul> */}
-                
+
 
                 {filters.map((section) => (
                   <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
@@ -253,13 +254,17 @@ export default function Product() {
                     </li>
                   ))}
                 </ul> */}
-                
 
-                {filters.map((section) => (
+
+
+
+                {filters.map((section) => ( //in that video used singleFilter 
                   <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
+
                     <h3 className="-my-3 flow-root">
                       <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">{section.name}</span>
+                        {/** <span className="font-medium "></span>*/}
+                        <FormLabel className='text-grey-900' id="demo-radio-buttons-group-label">{section.name}</FormLabel>
                         <span className="ml-6 flex items-center">
                           <PlusIcon aria-hidden="true" className="size-5 group-data-[open]:hidden" />
                           <MinusIcon aria-hidden="true" className="size-5 group-[&:not([data-open])]:hidden" />
@@ -268,47 +273,29 @@ export default function Product() {
                     </h3>
                     <DisclosurePanel className="pt-6">
                       <div className="space-y-4">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex gap-3">
-                            <div className="flex h-5 shrink-0 items-center">
-                              <div className="group grid size-4 grid-cols-1">
-                                <input
-                                  defaultValue={option.value}
-                                  defaultChecked={option.checked}
-                                  id={`filter-${section.id}-${optionIdx}`}
-                                  name={`${section.id}[]`}
-                                  type="checkbox"
-                                  className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-                                />
-                                <svg
-                                  fill="none"
-                                  viewBox="0 0 14 14"
-                                  className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-[:disabled]:stroke-gray-950/25"
-                                >
-                                  <path
-                                    d="M3 8L6 11L11 3.5"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-[:checked]:opacity-100"
-                                  />
-                                  <path
-                                    d="M3 7H11"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="opacity-0 group-has-[:indeterminate]:opacity-100"
-                                  />
-                                </svg>
-                              </div>
-                            </div>
-                            <label htmlFor={`filter-${section.id}-${optionIdx}`} className="text-sm text-gray-600">
-                              {option.label}
-                            </label>
-                          </div>
-                        ))}
+                        <FormControl>
+                          <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue="female"
+                            name="radio-buttons-group"
+                          >
+
+                            {section.options.map((option, optionIdx) => (
+
+
+                              <>
+                                <FormControlLabel value={option.id} control={<Radio />} label={option.label} />
+
+                              </>
+
+
+
+                            ))}
+                          </RadioGroup>
+                        </FormControl>
                       </div>
                     </DisclosurePanel>
+
                   </Disclosure>
                 ))}
               </form>
@@ -316,7 +303,7 @@ export default function Product() {
               {/* Product grid */}
               <div className="lg:col-span-4 w-full">
                 <div className='flex flex-wrap justify-center bg-white py-5'>
-                {mens_kurta.map((item)=><ProductCard product={item}/>)}
+                  {mens_kurta.map((item) => <ProductCard product={item} />)}
 
                 </div>
 
